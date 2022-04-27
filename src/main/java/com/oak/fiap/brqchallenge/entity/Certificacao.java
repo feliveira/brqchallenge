a7a350840a69b39631f0100d809e2d5f90cd5f27
feliveira <feliveira8@gmail.com>
2022-04-27 20:53:12 -0300

    Implementacao Certificacao

--- src/main/java/com/oak/fiap/brqchallenge/entity/Certificacao.java
+++ src/main/java/com/oak/fiap/brqchallenge/entity/Certificacao.java
@@ -11,4 +11,31 @@ public class Certificacao {
     @GeneratedValue(generator = "certificacao", strategy = GenerationType.SEQUENCE)
     private Long id;
 
+    private String nome;
+    private Skill tecnologia;
+
+    public Certificacao() {
+    }
+
+    public Certificacao(Long id, String nome, Skill tecnologia) {
+        this.id = id;
+        this.nome = nome;
+        this.tecnologia = tecnologia;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Skill getTecnologia() {
+        return tecnologia;
+    }
+    public void setTecnologia(Skill tecnologia) {
+        this.tecnologia = tecnologia;
+    }
 }
