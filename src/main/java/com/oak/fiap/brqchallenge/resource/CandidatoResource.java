6ac34816c8e49c051b312600043034c080c4086f
feliveira <feliveira8@gmail.com>
2022-04-27 20:59:33 -0300

    CandidatoResource findall e insert

--- src/main/java/com/oak/fiap/brqchallenge/resource/CandidatoResource.java
+++ src/main/java/com/oak/fiap/brqchallenge/resource/CandidatoResource.java
@@ -1,9 +1,14 @@
 package com.oak.fiap.brqchallenge.resource;
 
+import com.oak.fiap.brqchallenge.entity.Candidato;
 import com.oak.fiap.brqchallenge.repository.CandidatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/candidatos")
@@ -11,4 +16,16 @@ public class CandidatoResource {
 
     @Autowired
     CandidatoRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Candidato>> findAll() {
+        return ResponseEntity.ok().body(repository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Candidato> insert(@RequestBody Candidato candidato) {
+        candidato = repository.save(candidato);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(candidato.getId()).toUri();
+        return ResponseEntity.created(uri).body(candidato);
+    }
 }
