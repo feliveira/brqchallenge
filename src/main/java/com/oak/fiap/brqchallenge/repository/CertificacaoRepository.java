ddefeb7a96d2dfe3c363d7d617447138eef6ccd7
feliveira <feliveira8@gmail.com>
2022-04-27 21:16:59 -0300

    Certificacao findBy nome

--- src/main/java/com/oak/fiap/brqchallenge/repository/CertificacaoRepository.java
+++ src/main/java/com/oak/fiap/brqchallenge/repository/CertificacaoRepository.java
@@ -4,6 +4,10 @@ import com.oak.fiap.brqchallenge.entity.Certificacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CertificacaoRepository extends JpaRepository<Certificacao, Long> {
+
+    List<Certificacao> findAllByNomeContainingIgnoreCase(String nome);
 }
--- src/main/java/com/oak/fiap/brqchallenge/resource/CertificacaoResource.java
+++ src/main/java/com/oak/fiap/brqchallenge/resource/CertificacaoResource.java
@@ -1,9 +1,15 @@
 package com.oak.fiap.brqchallenge.resource;
 
+import com.oak.fiap.brqchallenge.entity.Certificacao;
 import com.oak.fiap.brqchallenge.repository.CertificacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("certificacoes")
@@ -12,4 +18,13 @@ public class CertificacaoResource {
     @Autowired
     CertificacaoRepository repository;
 
+    @GetMapping
+    public ResponseEntity<List<Certificacao>> findAll(){
+        return ResponseEntity.ok().body(repository.findAll());
+    }
+
+    @GetMapping("/findBy/{nome}")
+    public ResponseEntity<List<Certificacao>> findByNome(@PathVariable("nome") String nome){
+        return ResponseEntity.ok().body(repository.findAllByNomeContainingIgnoreCase(nome));
+    }
 }
