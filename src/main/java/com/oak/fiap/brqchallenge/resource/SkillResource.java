36e20c7a82bfd86d7e1f90450d0b4ae28a69c48f
feliveira <feliveira8@gmail.com>
2022-04-27 21:13:27 -0300

    SkillResource findBy

--- src/main/java/com/oak/fiap/brqchallenge/resource/SkillResource.java
+++ src/main/java/com/oak/fiap/brqchallenge/resource/SkillResource.java
@@ -1,9 +1,15 @@
 package com.oak.fiap.brqchallenge.resource;
 
+import com.oak.fiap.brqchallenge.entity.Skill;
 import com.oak.fiap.brqchallenge.repository.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/skills")
@@ -11,4 +17,14 @@ public class SkillResource {
 
     @Autowired
     SkillRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Skill>> findAll() {
+        return ResponseEntity.ok().body(repository.findAll());
+    }
+    @GetMapping("/findByNome/{nome}")
+    public ResponseEntity<List<Skill>> findByNome(@PathVariable("nome") String nome) {
+        return ResponseEntity.ok().body(repository.findAllByNomeContainingIgnoreCase(nome));
+    }
+
 }
